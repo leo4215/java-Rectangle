f9879c2e01de90a0eff482e48756a83b20c1d9c1
Leonardo de Camargo <134113848+leo4215@users.noreply.github.com>
2023-10-26 15:23:51 -0400

    Create Rectangle.java

---
+++ Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle {
+    public double width, height;
+
+    public double area(){
+        return width * height;
+    }
+
+    public double perimeter(){
+        return 2*(width + height);
+    }
+
+    public double diagonal(){
+        return Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    }
+
+    public String toString(){
+        return "AREA = " + String.format("%.2f%n", area()) + "PERIMETER = " + String.format("%.2f%n", perimeter()) + "DIAGONAL = " + String.format("%.2f", diagonal()); 
+    }
+}
